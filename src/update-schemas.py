2805edbb0fb43a9675dd35e2a7a6a7d02773700a
Sorin Sbarnea <ssbarnea@redhat.com>
2022-05-18 16:42:46 +0100

    Remove unused definitions from subschemas

--- src/update-schemas.py
+++ src/update-schemas.py
@@ -3,8 +3,12 @@ import copy
 import keyword
 import json
 import sys
+from typing import Any
 
-play_keywords = list(filter(None, """\
+play_keywords = list(
+    filter(
+        None,
+        """\
 any_errors_fatal
 become
 become_exe
@@ -46,41 +50,94 @@ timeout
 vars
 vars_files
 vars_prompt
-""".split()))
+""".split(),
+    )
+)
 
 
-invalid_var_names = sorted(list(keyword.kwlist) + play_keywords)
-print("Updating invalid var names")
+def is_ref_used(obj: Any, ref: str) -> bool:
+    """Return a reference use from a schema."""
+    ref_use = f"#/$defs/{ref}"
+    if isinstance(obj, dict):
+        if obj.get("$ref", None) == ref_use:
+            # breakpoint()
+            return True
+        for value in obj.values():
+            if isinstance(value, (dict, list)):
+                if is_ref_used(value, ref):
+                    # breakpoint()
+                    return True
+    elif isinstance(obj, list):
+        for value in obj:
+            # if value.get("$ref", None) == ref:
+            #     return True
+            if isinstance(value, (dict, list)):
+                if is_ref_used(value, ref):
+                    # breakpoint()
+                    return True
+    return False
 
-with open("f/ansible-vars.json", "r+", encoding="utf-8") as f:
-    vars_schema = json.load(f)
-    vars_schema['anyOf'][0]['patternProperties'] = {
-        f"^(?!({'|'.join(invalid_var_names)})$)[a-zA-Z_][\\w]*$": {}
-    }
-    f.seek(0)
-    json.dump(vars_schema, f, indent=2)
-    f.write("\n")
-    f.truncate()
 
-print("Compiling subschemas...")
-with open("f/ansible.json", encoding="utf-8") as f:
-    combined_json = json.load(f)
+if __name__ == "__main__":
 
-for subschema in ['tasks', 'playbook']:
-    sub_json = copy.deepcopy(combined_json)
-    # remove unsafe keys from root
-    for key in ['$id', 'id', 'title', 'description', 'type', 'default','items', 'properties', 'additionalProperties', 'examples']:
-        if key in sub_json:
-            del sub_json[key]
-    for key in sub_json:
-        if key not in ['$schema', '$defs']:
-            print(f"Unexpected key found at combined schema root: ${key}")
-            sys.exit(2)
-    # Copy keys from subschema to root
-    for key, value in combined_json['$defs'][subschema].items():
-        sub_json[key] = value
-    sub_json['$comment'] = 'Generated from ansible.json, do not edit.'
-    sub_json['$id'] = f"https://raw.githubusercontent.com/ansible/schemas/main/f/ansible-{subschema}.json"
-    with open(f"f/ansible-{subschema}.json", "w") as f:
-        json.dump(sub_json, f, indent=2, sort_keys=True)
-        f.write('\n')
+    invalid_var_names = sorted(list(keyword.kwlist) + play_keywords)
+    print("Updating invalid var names")
+
+    with open("f/ansible-vars.json", "r+", encoding="utf-8") as f:
+        vars_schema = json.load(f)
+        vars_schema["anyOf"][0]["patternProperties"] = {
+            f"^(?!({'|'.join(invalid_var_names)})$)[a-zA-Z_][\\w]*$": {}
+        }
+        f.seek(0)
+        json.dump(vars_schema, f, indent=2)
+        f.write("\n")
+        f.truncate()
+
+    print("Compiling subschemas...")
+    with open("f/ansible.json", encoding="utf-8") as f:
+        combined_json = json.load(f)
+
+    for subschema in ["tasks", "playbook"]:
+        sub_json = copy.deepcopy(combined_json)
+        # remove unsafe keys from root
+        for key in [
+            "$id",
+            "id",
+            "title",
+            "description",
+            "type",
+            "default",
+            "items",
+            "properties",
+            "additionalProperties",
+            "examples",
+        ]:
+            if key in sub_json:
+                del sub_json[key]
+        for key in sub_json:
+            if key not in ["$schema", "$defs"]:
+                print(f"Unexpected key found at combined schema root: ${key}")
+                sys.exit(2)
+        # Copy keys from subschema to root
+        for key, value in combined_json["$defs"][subschema].items():
+            sub_json[key] = value
+        sub_json["$comment"] = "Generated from ansible.json, do not edit."
+        sub_json[
+            "$id"
+        ] = f"https://raw.githubusercontent.com/ansible/schemas/main/f/ansible-{subschema}.json"
+
+        # Remove all unreferenced ($ref) definitions ($defs) recursively
+        while True:
+            spare = []
+            for k in sub_json["$defs"].keys():
+                if not is_ref_used(sub_json, k):
+                    spare.append(k)
+            for k in spare:
+                print(f"{subschema}: deleting unused '{k}' definition")
+                del sub_json["$defs"][k]
+            if not spare:
+                break
+
+        with open(f"f/ansible-{subschema}.json", "w", encoding="utf-8") as f:
+            json.dump(sub_json, f, indent=2, sort_keys=True)
+            f.write("\n")
